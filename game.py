a71f0e66258e8c020cedc6117e58f82db9d417f3
James <jajrynn@googlemail.com>
2020-04-13 16:02:42 +0100

    finished commenting game.py

--- game.py
+++ game.py
@@ -8,7 +8,7 @@ import os
 
 def mouse_to_index(mx, my, buffer, tile_size):
     """
-    Converts the mouse click location into a board/tile index.
+    Converts a mouse click location into a board/tile index.
 
     Parameters:
     -----------
@@ -32,26 +32,42 @@ def mouse_to_index(mx, my, buffer, tile_size):
 
 def draw_board(game_display, buffer, tile_size, game, piece, move, move_locations, shoot_locations):
     """
-    FUNCTION DESCRIPTION.
+    Draws the current game board.
 
     Parameters:
     -----------
-
-    Returns:
-    --------
+    game_display : pygame.display
+        Instance of a pygame display.
+    buffer : float
+        Width of the buffer surrounding the game board.
+    tile_size : float
+        Width/height of a single tile.
+    game : Game
+        Instance of the Game class from utility.game_backend
+    piece: int
+        Tile position on the board of selected Warrior.
+    move : int
+        Tile position on the board for selected Warrior to move to.
+    move_locations : list
+        List of tile positions on the board to which the selected warrior may move.
+    shoot_locations : list
+        List of tile positions on the board which the selected warrior may shoot.
     """
-    flame_image = pygame.image.load('./utility/flame.png')          # image of flame
-    blue_player = pygame.image.load('./utility/blue_player.png')    # image of blue warrior
+    # Game piece .png images.
+    blue_player  = pygame.image.load('./utility/blue_player.png')   # image of blue warrior
     green_player = pygame.image.load('./utility/green_player.png')  # image of green warrior
+    flame_image  = pygame.image.load('./utility/flame.png')         # image of flame
 
+    # Pre-defined colours as RGB values.
     WHITE = (255,255,255)
-    GREY = (169,169,169)
-    RED = (255,0,0)
+    GREY  = (169,169,169)
+    RED   = (255,0,0)
     BLACK = (0,0,0)
 
+    # Draw the game board: white background.
     game_display.fill(WHITE)
 
-    # Draw board
+    # Draw the game board: fill every other diagonal in grey.
     for x in range(0, 10):
         if x%2 == 1:
             for y in range(0, 10, 2):
@@ -59,16 +75,20 @@ def draw_board(game_display, buffer, tile_size, game, piece, move, move_location
         else:
             for y in range(1, 10, 2):
                 pygame.draw.rect(game_display, GREY, (buffer+x*tile_size, buffer+y*tile_size, tile_size, tile_size))
-    pygame.draw.line(game_display, GREY, (buffer,buffer), (buffer,buffer+10*tile_size), 4)
-    pygame.draw.line(game_display, GREY, (buffer,buffer+10*tile_size), (buffer+10*tile_size,buffer+10*tile_size), 4)
-    pygame.draw.line(game_display, GREY, (buffer+10*tile_size,buffer+10*tile_size), (buffer+10*tile_size,buffer), 4)
-    pygame.draw.line(game_display, GREY, (buffer+10*tile_size,buffer), (buffer,buffer), 4)
 
-    # Add pieces, move_locations and shoot_locations
+    # Draw the game board: add grey border.
+    pygame.draw.line(game_display, GREY, (buffer,buffer),                           (buffer,buffer+10*tile_size), 4)                # bottom left  -> top left
+    pygame.draw.line(game_display, GREY, (buffer,buffer+10*tile_size),              (buffer+10*tile_size,buffer+10*tile_size), 4)   # top left     -> top right
+    pygame.draw.line(game_display, GREY, (buffer+10*tile_size,buffer+10*tile_size), (buffer+10*tile_size,buffer), 4)                # top right    -> bottom right
+    pygame.draw.line(game_display, GREY, (buffer+10*tile_size,buffer),              (buffer,buffer), 4)                             # bottom right -> bottom left
+
+    # Add pieces.
     piece_radius = int(0.4*tile_size)
     for col in range(10):
         for row in range(10):
-            index = row + (col)*10
+            # Position index of (row,column) pair.
+            index = row + col*10
+            # Top left hand corner of the tile whose position is index.
             top_left = (int(buffer+(tile_size*(int(row)))), int(buffer+(tile_size*(int(col)))))
 
             if index == piece and move != None:
@@ -86,21 +106,25 @@ def draw_board(game_display, buffer, tile_size, game, piece, move, move_location
                 elif game.board.game_tiles[index].alliance == 0:
                     game_display.blit(flame_image, top_left)
 
-    # Add moves - TODO: Replace with (transparent) warrior symbols?
+    # Add move locations. - TODO: Replace circle with (transparent) warrior symbols?
     if not move_locations == None:
         for move in move_locations:
+            # Co-ordinates of the centre of the tile.
             location = (int(buffer+ tile_size*(move%10 + 0.5)), int(buffer+ tile_size*(move//10 + 0.5)))
+            # Draw black circles to indicate possible move locations.
             pygame.draw.circle(game_display, BLACK, location, 5, 5)
 
-    # Add shoot - TODO: Replace with target images?
+    # Add shoot locations. - TODO: Replace circle with target images?
     if not shoot_locations == None:
         for s in shoot_locations:
+            # Co-ordinates of the centre of the tile.
             location = (int(buffer+ tile_size*(s%10 + 0.5)), int(buffer+ tile_size*(s//10 + 0.5)))
+            # Draw red circles to indicate possible shoot locations.
             pygame.draw.circle(game_display, RED, location, 5, 5)
     return
 
 
-def main(testgame=False):
+def main(testgame=True):
     """
     FUNCTION DESCRIPTION.
 
@@ -164,9 +188,9 @@ def main(testgame=False):
         # If no available plays, end the game and update caption.
         if available_plays == {} or g.turncount == 3: # <---- TODO: REMOVE
             if g.turn == '1':
-                pygame.display.set_caption('Game of the Amazons - Green has won the game in ' + str(g.turncount) + ' turns! Click anywhere to close the game.')
+                pygame.display.set_caption('Game of the Amazons - Green has won the game in ' + str(g.turncount-1) + ' turns! Click anywhere to close the game.')
             else:
-                pygame.display.set_caption('Game of the Amazons - Blue has won the game in '  + str(g.turncount) + ' turns! Click anywhere to close the game.')
+                pygame.display.set_caption('Game of the Amazons - Blue has won the game in '  + str(g.turncount-1) + ' turns! Click anywhere to close the game.')
             """
             TODO: Have pop up message rather than caption change.
             # If game over display message to screen.
@@ -194,6 +218,7 @@ def main(testgame=False):
                 run_game = False
 
             # If a test game, complete a random play.
+            # Note requires three passes through the while loop per play due to board being drawn at end.
             if testgame:
                 if piece == None:
                     # Select random play.
@@ -203,71 +228,71 @@ def main(testgame=False):
 
                     # Select a piece.
                     x, y = int(buffer + tile_size*(r_piece%10 + 0.5)), int(buffer + tile_size*(r_piece//10 + 0.5))
-                    mouse.position = (x, y+5)
+                    mouse.position = (x, y)
                     mouse.click(Button.left, 1)
 
                 elif piece != None and move == None:
                     # Select a move position.
                     x, y = int(buffer + tile_size*(r_move%10 + 0.5)), int(buffer + tile_size*(r_move//10 + 0.5))
-                    mouse.position = (x, y+5)
+                    mouse.position = (x, y)
                     mouse.click(Button.left, 1)
 
                 elif piece != None and move != None:
                     # Select a shoot location.
                     x, y = int(buffer + tile_size*(r_shoot%10 + 0.5)), int(buffer + tile_size*(r_shoot//10 + 0.5))
-                    mouse.position = (x, y+5)  ## TODO: should this be (x,y+5)?
+                    mouse.position = (x, y)  ## TODO: should this be (x,y+5)?
                     mouse.click(Button.left, 1)
 
             # Not a random game, therefore request user input.
-            else:
-                # User has pressed the mouse button to select a tile.
-                if event.type == pygame.MOUSEBUTTONDOWN:
-                    # Mouse cursor position.
-                    mx, my = pygame.mouse.get_pos()
-                    # Find corresponding tile index.
-                    index = mouse_to_index(mx, my, buffer, tile_size)
+            #else:
+            # User has pressed the mouse button to select a tile.
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                # Mouse cursor position.
+                mx, my = pygame.mouse.get_pos()
+                # Find corresponding tile index.
+                index = mouse_to_index(mx, my, buffer, tile_size)
 
-                    # Warrior piece of current team selected using the mouse.
-                    if g.board.game_tiles[index].to_string() == g.turn and move == None:
-                        # Selected tile index is the index of a Warrior.
-                        piece = index
-                        # Find (and highlight below) all legal moves for this Warrior.
-                        move_locations = g.board.game_tiles[piece].find_moves_or_shots(g.board, piece, piece)
+                # Warrior piece of current team selected using the mouse.
+                if g.board.game_tiles[index].to_string() == g.turn and move == None:
+                    # Selected tile index is the index of a Warrior.
+                    piece = index
+                    # Find (and highlight below) all legal moves for this Warrior.
+                    move_locations = g.board.game_tiles[piece].find_moves_or_shots(g.board, piece, piece)
 
-                    # If piece has been selected, require move or shoot choice.
-                    elif piece != None:
-                        # If piece has not yet moved and a valid move is selected.
-                        if index in move_locations and move == None:
-                            # Selected tile index is the index of a legal move position.
-                            move = index
-                            # Find (and highlight below) all legal shoot locations for this move.
-                            shoot_locations = g.board.game_tiles[piece].find_moves_or_shots(g.board, move, piece, shooting=True)
-                            # Reset move_locations variable.
-                            move_locations = []
+                # If piece has been selected, require move or shoot choice.
+                elif piece != None:
+                    # If piece has not yet moved and a valid move is selected.
+                    if index in move_locations and move == None:
+                        # Selected tile index is the index of a legal move position.
+                        move = index
+                        # Find (and highlight below) all legal shoot locations for this move.
+                        shoot_locations = g.board.game_tiles[piece].find_moves_or_shots(g.board, move, piece, shooting=True)
+                        # Reset move_locations variable.
+                        move_locations = []
 
-                        # If piece has moved but not shot, and a legal shot is selected.
-                        elif index in shoot_locations and move != None:
-                            # Complete valid play.
-                            g.make_play(piece, move, index)
-                            # Update caption to let user know whose turn it is.
-                            if g.turn == '1':
-                                pygame.display.set_caption('Game of the Amazons - Shot fired by Green!')
-                            else:
-                                pygame.display.set_caption('Game of the Amazons - Shot fired by Blue!')
-                            # Reset the play parameters.
-                            piece           = None
-                            move            = None
-                            move_locations  = []
-                            shoot_locations = []
+                    # If piece has moved but not shot, and a legal shot is selected.
+                    elif index in shoot_locations and move != None:
+                        # Complete valid play.
+                        g.make_play(piece, move, index)
+                        # Update caption to let user know whose turn it is.
+                        if g.turn == '1':
+                            pygame.display.set_caption('Game of the Amazons - Shot fired by Green!')
+                        else:
+                            pygame.display.set_caption('Game of the Amazons - Shot fired by Blue!')
+                        # Reset the play parameters.
+                        piece           = None
+                        move            = None
+                        move_locations  = []
+                        shoot_locations = []
 
 
-                ### **CHANGE TO UPDATE BOARD**
-
+                # TODO: Change to update board.
                 # Draw board
                 draw_board(game_display, buffer, tile_size, g, piece, move, move_locations, shoot_locations)
                 pygame.display.flip()
 
-    pygame.quit() # closes window after while loop condition is violated
+    # Closes the game window after the while loop condition is violated.
+    pygame.quit()
 
 
 
